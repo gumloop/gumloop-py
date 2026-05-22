b10ca0ea85391bf340b385b17365d66538518f41
Kaustav Das Sharma <42706537+kdassharma@users.noreply.github.com>
2026-05-22 04:47:18 -0700

    bump package minor version

--- src/gumloop/__init__.py
+++ src/gumloop/__init__.py
@@ -6,7 +6,7 @@ from gumloop.errors import AuthenticationError
 from gumloop.errors import GumloopError
 from gumloop.oauth import OAuth
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __all__ = [
     "APIStatusError",
     "AsyncGumloop",
